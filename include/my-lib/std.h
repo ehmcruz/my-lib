d1240590b7643e6529639e9932713fbd7caa5f34
Eduardo H. M. Cruz <eduardohmdacruz@gmail.com>
2025-02-14 17:01:35 -0300

    added enum concept

--- include/my-lib/std.h
+++ include/my-lib/std.h
@@ -36,6 +36,11 @@ using size_t = std::size_t;
 
 // ---------------------------------------------------
 
+template <typename T>
+concept Enum = std::is_enum_v<T>;
+
+// ---------------------------------------------------
+
 constexpr void build_str_from_stream__ (std::ostringstream& str_stream) noexcept
 {
 }
