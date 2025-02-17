a04d3fcf61ac56117bf1ab229108a3000d26baac
Eduardo H. M. Cruz <eduardohmdacruz@gmail.com>
2025-02-17 14:17:37 -0300

    writting interpolation manager

--- include/my-lib/interpolation.h
+++ include/my-lib/interpolation.h
@@ -1,7 +1,12 @@
 #ifndef __MY_LIB_INTERPOLATION_HEADER_H__
 #define __MY_LIB_INTERPOLATION_HEADER_H__
 
+#include <variant>
+
 #include <my-lib/std.h>
+#include <my-lib/trigger.h>
+#include <my-lib/coroutine.h>
+#include <my-lib/memory.h>
 
 
 namespace Mylib
@@ -57,7 +62,7 @@ protected:
 	Ty& target;
 
 public:
-	Interpolator__ (const Tx max_x_, Ty *target_, Ty start_y_)
+	Interpolator__ (const Tx max_x_, Ty *target_, const Ty start_y_)
 		: Interpolator<Tx>(max_x_),
 		target(*target_)
 	{
@@ -75,7 +80,7 @@ protected:
 	Ty rate;
 
 public:
-	LinearInterpolator (const Tx max_x_, Ty *target_, Ty start_y_, Ty end_y_)
+	LinearInterpolator (const Tx max_x_, Ty *target_, const Ty start_y_, const Ty end_y_)
 		: Interpolator__<Tx, Ty>(max_x_, target_, start_y_),
 		  start_y(start_y_),
 		  rate((end_y_ - start_y_) / max_x_)
@@ -86,6 +91,72 @@ protected:
 	void interpolate (const Tx x) override final
 	{
 		this->target = this->start_y + x * this->rate;
+	}
+};
+
+// ---------------------------------------------------
+
+template <typename Tx>
+class InterpolationManager
+{
+public:
+	struct Event : public Mylib::Coroutine::Event {
+		Interpolator<Tx> *interpolator;
+	};
+
+	struct Descriptor {
+		Event *ptr;
+	};
+
+private:
+	using InterpolatorCallback = Mylib::Trigger::Callback<Event>;
+
+	struct EventCallback {
+		InterpolatorCallback *callback;
+	};
+
+	struct EventCoroutine {
+		CoroutineHandle coroutine_handler;
+	};
+
+	struct EventFull : public Event {
+		std::variant<EventCallback, EventCoroutine> var_callback;
+		bool enabled;
+	};
+
+	Memory::Manager& memory_manager;
+	std::vector<EventFull*> interpolators;
+
+public:
+	InterpolationManager ()
+		: memory_manager(Memory::default_manager)
+	{
+	}
+
+	InterpolationManager (Memory::Manager& memory_manager_)
+		: memory_manager(memory_manager_)
+	{
+	}
+
+	template <typename Ty>
+	Descriptor interpolate_linear (const Tx max_x_, Ty *target_, const Ty start_y_, const Ty end_y_, const InterpolatorCallback& callback)
+	{
+		EventFull *event = new (this->memory_manager.template allocate_type<EventFull>(1)) EventFull;
+		event->interpolator = new (this->memory_manager.template allocate_type<LinearInterpolator<Tx, Ty>>(1)) LinearInterpolator<Tx, Ty>(max_x_, target_, start_y_, end_y_);
+		event->var_callback = EventCallback {
+			.callback = callback.make_copy(this->memory_manager),
+		},
+		event->enabled = true;
+
+		this->push(event);
+		
+		return Descriptor { .ptr = event };
+	}
+
+private:
+	inline void push (EventFull *event)
+	{
+		this->events.push_back(event);
 	}
 };
 
